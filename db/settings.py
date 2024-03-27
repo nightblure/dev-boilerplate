1f874203b8f5a7bf3db8ba695757e04c809042ef
Ivan Belyaev <vanobel159@gmail.com>
2024-03-27 14:40:44 +0300

    Create settings.py

---
+++ db/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DbSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix='DB_',
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=False
+    )
+
+    db_url: str = 'sqlite:///db.db
+    is_need_log_sql: bool = True
