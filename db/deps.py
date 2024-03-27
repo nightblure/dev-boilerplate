9631005fdd5e1c8dfad3c0cd9ed87111add7b180
Ivan Belyaev <vanobel159@gmail.com>
2024-03-27 14:25:23 +0300

    Create deps.py

---
+++ db/deps.py
@@ -0,0 +1,58 @@
+from asyncio import iscoroutinefunction
+from typing import ContextManager
+
+from db.session_manager import DbSessionManager
+from db.typings import SQLADbSession
+
+settings = ...
+
+
+def create_db_session_manager() -> DbSessionManager:
+    if DbSessionManager.instance is None:
+        db_session_manager_kwargs = dict(
+            pool_size=20,
+            max_overflow=0,
+            pool_pre_ping=True,
+            echo=settings.is_need_log_sql,
+            scoped=False,
+            db_url=settings.database
+        )
+
+        DbSessionManager.instance = DbSessionManager(**db_session_manager_kwargs)
+
+    return DbSessionManager.instance
+
+
+def get_db_session(*, from_cache: bool = True) -> SQLADbSession:
+    return create_db_session_manager().get_db_session(from_cache=from_cache)
+
+
+def get_db_session_context(*, from_cache: bool = True) -> ContextManager[SQLADbSession]:
+    return create_db_session_manager().get_db_session_context(from_cache=from_cache)
+
+
+def inject_db_session():
+    """Inject session into decorated function. Name for the session arg must be 'db_session'"""
+
+    def wrapper(f):
+
+        if iscoroutinefunction(f):
+            async def inner(*a, **kw):
+                with get_db_session_context() as db_session:
+                    kw['db_session'] = db_session
+                    result = await f(*a, **kw)
+
+                return result
+
+            return inner
+        else:
+            def inner(*a, **kw):
+                with get_db_session_context() as db_session:
+                    kw['db_session'] = db_session
+                    result = f(*a, **kw)
+
+                return result
+
+            return inner
+
+    return wrapper
