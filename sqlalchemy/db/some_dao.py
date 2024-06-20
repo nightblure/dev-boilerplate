983db98fe77677572a8de8681bdaba58cca8dc45
Ivan Belyaev <vanobel159@gmail.com>
2024-06-20 16:33:37 +0300

    Create some_dao.py

---
+++ sqlalchemy/db/some_dao.py
@@ -0,0 +1,13 @@
+from db.deps import get_db_session_context, get_db_session, DbSession
+from db.typings import SQLADbSession
+from db.sqla_mixin import SQLAMixin
+
+
+class SomeDAO(SQLAMixin):
+    model = SomeModel
+  
+    def __init__(self, db_session: SQLADbSession):
+        self.db_session = db_session
+
+    def get_all(self):
+        return self.all()
