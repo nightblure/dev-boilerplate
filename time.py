cea9936e88c660d898f98139b9bf650bc780d09b
Ivan Belyaev <vanobel159@gmail.com>
2024-08-28 16:17:32 +0300

    Update time.py

--- time.py
+++ time.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone, date
+from datetime import date, datetime, timezone
 
 
 def datetime_to_utc(d: datetime) -> datetime:
@@ -6,11 +6,7 @@ def datetime_to_utc(d: datetime) -> datetime:
 
 
 def date_to_datetime(d: date, *, to_utc: bool = False) -> datetime:
-    dtime = datetime(
-        year=d.year,
-        month=d.month,
-        day=d.day
-    )
+    dtime = datetime(year=d.year, month=d.month, day=d.day)
 
     if to_utc:
         dtime = datetime_to_utc(dtime)
@@ -18,11 +14,16 @@ def date_to_datetime(d: date, *, to_utc: bool = False) -> datetime:
     return dtime
 
 
-def get_utc_now(format=True):
-    utc_now = datetime.now(tz=timezone.utc)
-    if format:
-        utc_now = utc_now.strftime('%Y-%m-%d %H:%M:%S')
-    return utc_now
+def get_utc_now() -> datetime:
+    return datetime.now(tz=timezone.utc)
+
+
+def datetime_to_string(dt: datetime, *, fmt: str = '%Y-%m-%d %H:%M:%S') -> str:
+    return dt.strftime(fmt)
+
+
+def create_datetime_from_string(dt_str: str, *, fmt: str = '%Y-%m-%d %H:%M:%S') -> datetime:
+    return datetime.strptime(dt_str, fmt)
 
 
 def date_to_iso_datetime(d: date, *, to_utc: bool = True) -> str:
