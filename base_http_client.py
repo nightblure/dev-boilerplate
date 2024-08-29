202a394112588db1042b2308add2ba57d1bbb81e
Ivan Belyaev <vanobel159@gmail.com>
2024-08-29 18:14:14 +0300

    Update base_http_client.py

--- base_http_client.py
+++ base_http_client.py
@@ -3,36 +3,69 @@ from typing import Any
 
 import httpx
 from loguru import logger
-from requests import Response
-
-# from some_project.exceptions import ExternalServiceError, ExternalServiceTimeoutError, SomeServiceError
-from decorators import log_if_errors
 
 
-class SomeServiceError(Exception):
-    def __init__(self, message: str, status_code: int = 500):
+class ExternalServiceError(Exception):
+    def __init__(self, *, log_message: str, message: str, status_code: int, host: str):
+        self.host = host
         self.message = message
+        self.log_message = log_message
         self.status_code = status_code
 
+    def __str__(self):
+        return self.message
 
-class ExternalServiceError(SomeServiceError):
-    status_code = 500
 
-    def __init__(self, host_or_svc_name: str):
+class ExternalServiceInternalError(ExternalServiceError):
+    def __init__(self, *, host: str, status_code: int = 500):
+        log_message = f'External service {host!r} unavailable. Reason: {status_code}'
         super().__init__(
-            f'External service "{host_or_svc_name}" unavailable', self.status_code
+            message=f'External service {host!r} unavailable',
+            status_code=status_code,
+            host=host,
+            log_message=log_message,
         )
 
 
-class ExternalServiceTimeoutError(SomeServiceError):
-    status_code = 500
+class ExternalServiceTimeoutError(ExternalServiceError):
+    def __init__(self, host: str):
+        message = f'Timeout error while connecting to host {host!r}'
+        super().__init__(
+            log_message=message,
+            message=message,
+            status_code=504,
+            host=host,
+        )
 
-    def __init__(self, host_or_svc_name: str):
-        super().__init__(f'Timeout error while connecting to "{host_or_svc_name}"', self.status_code)
+
+class ExternalServiceClientError(ExternalServiceError):
+    def __init__(self, *, status_code: int = 400, host: str, content):
+        log_message = (
+            f'Client error while connecting to {host!r}.\n'
+            f'Content: {content}.\n'
+            f'Status code: {status_code}'
+        )
+        super().__init__(
+            log_message=log_message,
+            message=f'Client error while connecting to {host!r}',
+            status_code=status_code,
+            host=host,
+        )
+
+
+class ExternalServiceUnknownError(ExternalServiceError):
+    def __init__(self, host: str):
+        message = f'Unknown error while connecting to host {host!r}'
+        super().__init__(
+            message=message,
+            log_message=message,
+            status_code=500,
+            host=host,
+        )
 
 
 class Result:
-    def __init__(self, response: Response):
+    def __init__(self, response: httpx.Response):
         self.response = response
         self.status_code = response.status_code
 
@@ -44,8 +77,27 @@ class Result:
             self.is_json = False
 
         self.ok = 200 <= response.status_code < 300
-        self.client_error = 400 <= response.status_code < 500
-        self.server_error = 500 <= response.status_code < 600
+        self.client_error = 400 <= response.status_code <= 499
+        self.server_error = 500 <= response.status_code <= 599
+        self.host = f'{response.url.scheme}://{response.url.host}'
+
+    def __repr__(self):
+        return f'Result(status_code={self.status_code})'
+
+
+def _raise_for_status(result: Result):
+    if result.client_error:
+        raise ExternalServiceClientError(
+            status_code=result.status_code,
+            host=result.host,
+            content=result.content,
+        )
+
+    if result.server_error:
+        raise ExternalServiceInternalError(
+            host=result.host,
+            status_code=result.status_code,
+        )
 
 
 class BaseHttpClient:
@@ -71,12 +123,11 @@ class BaseHttpClient:
         self.password = password
         self.auth_endpoint = auth_endpoint
 
-        self.default_headers = {
+        self._default_request_headers = {
             'User-Agent': app_name,
-            'Content-Type': 'application/json',
             'Accept': 'application/json',
+            'Content-Type': 'application/json',
         }
-        self.token = None
 
     def make_url(self, endpoint: str):
         if self.api_version == '':
@@ -92,24 +143,26 @@ class BaseHttpClient:
         endpoint: str,
         query_params: dict[str, Any] | None = None,
         body: dict[str, Any] | None = None,
-        headers=None,
+        headers: dict[str, Any] | None = None,
         timeout: int = TIMEOUT,
         retries: int = 1,
         retries_timeout: int = RETRIES_TIMEOUT,
         follow_redirects=False,
+        raise_for_status: bool = True,
+        retry_on_403: bool = False,
     ) -> Result:
         url = self.make_url(endpoint)
 
         if headers is None:
             headers = {}
 
-        headers = {**self.default_headers, **headers}
+        headers = {**self._default_request_headers, **headers}
         current_retries_timeout = retries_timeout
         current_backoff = 1
 
         for i in range(retries):
             try:
-                result = await self._send_request(
+                result = await self.__send_request(
                     method,
                     url=url,
                     query_params=query_params,
@@ -117,12 +170,16 @@ class BaseHttpClient:
                     headers=headers,
                     timeout=timeout,
                     follow_redirects=follow_redirects,
+                    raise_for_status=raise_for_status,
                 )
 
                 return result
 
-            # We SHOULDN'T to retry client errors (4XX)
-            except (ExternalServiceError, ExternalServiceTimeoutError):
+            except ExternalServiceError as e:
+                # We SHOULDN'T to retry client errors (4XX)
+                if 400 <= e.status_code < 500:
+                    raise
+
                 if retries == 1:
                     raise
 
@@ -138,9 +195,9 @@ class BaseHttpClient:
                 current_backoff *= self.RETRIES_BACKOFF_FACTOR
                 continue
 
-        raise ExternalServiceError(self.host)
+        raise ExternalServiceUnknownError(self.host)
 
-    async def _send_request(
+    async def __send_request(
         self,
         method: str,
         *,
@@ -150,7 +207,8 @@ class BaseHttpClient:
         headers=None,
         timeout: int,
         follow_redirects=False,
-    ):
+        raise_for_status: bool = True,
+    ) -> Result:
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.request(
@@ -163,41 +221,27 @@ class BaseHttpClient:
                     follow_redirects=follow_redirects,
                 )
 
-                if response.is_success:
-                    return Result(response)
+                result = Result(response)
 
-                if response.is_client_error:
-                    msg = (
-                        f'Client error while connecting to "{self.host}". '
-                        f'Content: {response.content}; status code: {response.status_code}'
-                    )
+                if response.is_success or not raise_for_status:
+                    return result
 
-                    logger.error(msg)
-
-                    if response.status_code in (401, 403):
-                        return Result(response)
-
-                    raise PixiError(response.content.decode(), response.status_code)
-
-                if response.is_server_error:
-                    logger.error(
-                        f'Internal server error on "{self.host}: {response.content!s}"',
-                    )
-                    raise ExternalServiceError(self.host)
+                if raise_for_status:
+                    _raise_for_status(result)
 
         except httpx.TimeoutException as e:
             msg = f'Timeout error while connecting to "{self.host}"'
             logger.exception(msg)
             raise ExternalServiceTimeoutError(self.host) from e
 
-    async def get(self, **kw):
+    async def get(self, **kw) -> Result:
         """See available arguments in request method"""
         return await self.request('get', **kw)
 
-    async def post(self, **kw):
+    async def post(self, **kw) -> Result:
         """See available arguments in request method"""
         return await self.request('post', **kw)
 
-    async def put(self, **kw):
+    async def put(self, **kw) -> Result:
         """See available arguments in request method"""
         return await self.request('put', **kw)
