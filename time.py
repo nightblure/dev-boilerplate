44b69bd482b09511c300ba005c0ec2a89a86a72f
Ivan Belyaev <vanobel159@gmail.com>
2024-08-29 18:26:51 +0300

    Update time.py

--- time.py
+++ time.py
@@ -5,10 +5,10 @@ def datetime_to_utc(d: datetime) -> datetime:
     return d.astimezone(tz=UTC)
 
 
-def date_to_datetime(d: date, *, to_utc: bool = False) -> datetime:
+def date_to_datetime(d: date, *, utc: bool = False) -> datetime:
     dtime = datetime(year=d.year, month=d.month, day=d.day)
 
-    if to_utc:
+    if utc:
         dtime = datetime_to_utc(dtime)
 
     return dtime
@@ -22,6 +22,10 @@ def datetime_to_string(dt: datetime, *, fmt: str = '%Y-%m-%d %H:%M:%S') -> str:
     return dt.strftime(fmt)
 
 
+def datetime_to_rus_str_format(dt: datetime) -> str:
+    return dt.strftime('%d.%m.%Y %H:%M:%S')
+
+
 def create_datetime_from_string(
     dt_str: str,
     *,
@@ -30,8 +34,8 @@ def create_datetime_from_string(
     return datetime.strptime(dt_str, fmt)
 
 
-def date_to_iso_datetime(d: date, *, to_utc: bool = True) -> str:
-    d_datetime = date_to_datetime(d, to_utc=to_utc)
+def date_to_iso_datetime(d: date, *, utc: bool = True) -> str:
+    d_datetime = date_to_datetime(d, utc=utc)
     d_iso = d_datetime.isoformat()
     return d_iso
 
@@ -40,5 +44,10 @@ def unix_timestamp_from_datetime(dt: datetime) -> int:
     return int(dt.strftime('%s'))
 
 
-def datetime_from_unix_timestamp(unix_timestamp: int) -> datetime:
-    return datetime.fromtimestamp(unix_timestamp, tz=UTC)
+def datetime_from_unix_timestamp(unix_timestamp: int, *, utc: bool = True) -> datetime:
+    tz = None
+
+    if utc:
+        tz = UTC
+
+    return datetime.fromtimestamp(unix_timestamp, tz=tz)
